63ffa8b45d8fc0da22929a302bebfab9fb9708bb
Mohit105 <88244415+Mohit105@users.noreply.github.com>
2021-08-10 13:12:00 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("My First Console Project");
+var first_name="Ranbir";
+var last_name="Kapoor";
+console.log(f_name.concat(l_name));
+var a = 10;
+var b = 5;
+var add = a + b;
+console.log(add);
+var subtract = a - b;
+console.log(subtract);
